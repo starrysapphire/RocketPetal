9346c82f5d0bbd9ae641cb21a1db62bc6b1ed53c
starrysapphire <stbrodri@ucsc.edu>
2021-04-09 13:55:39 -0700

    Made Starfield Background Scroll Infinitely!

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,7 +11,7 @@ class Play extends Phaser.Scene {
         this.starfield = this.add.tileSprite(
             0,0, 640,480, 'starfield'
         ).setOrigin(0,0);
-        
+
         //green UI background
         this.add.rectangle (
             0,
@@ -25,5 +25,9 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0); 
+    }
+
+    update() {
+        this.starfield.tilePositionX += 4;
     }
 }
