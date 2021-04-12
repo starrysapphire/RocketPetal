5f48f564b5d4203b3f58435bea2028e317d8ec87
starrysapphire <stbrodri@ucsc.edu>
2021-04-12 14:00:25 -0700

    added controlable p1 rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,12 +5,20 @@ class Play extends Phaser.Scene {
 
     preload () {
         this.load.image('starfield', 'assets/starfield.png');
+        this.load.image('rocket', 'assets/rocket.png');
     }
 
     create () {
         this.starfield = this.add.tileSprite(
             0,0, 640,480, 'starfield'
         ).setOrigin(0,0);
+
+        //add rocket (p1, since originally multiplayer game)
+        this.p1rocket = new Rocket(this, 
+            game.config.width/2,
+            game.config.height - borderUISize - borderPadding,
+            'rocket'
+        );
 
         //green UI background
         this.add.rectangle (
@@ -24,10 +32,17 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0); 
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+        
+          // define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update() {
-        this.starfield.tilePositionX += 4;
+        this.starfield.tilePositionX -= 4;
+        this.p1rocket.update();
     }
 }
