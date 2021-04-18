b77b825ee0dfefa10c33f8b6d8bf6bb267b046ae
starrysapphire <stbrodri@ucsc.edu>
2021-04-17 21:28:41 -0700

    testing cloned repo

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -39,5 +39,6 @@ class Rocket extends Phaser.GameObjects.Sprite {
     reset() {
         this.isFiring = false;
         this.y = game.config.height - borderUISize - borderPadding;
+        console.log("hello");
     }
 }
