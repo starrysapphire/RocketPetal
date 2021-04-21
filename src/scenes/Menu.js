854ec205a434491f07d832e264420c0b8ef778ad
starrysapphire <stbrodri@ucsc.edu>
2021-04-20 18:36:07 -0700

    menu screen displays

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,36 +5,20 @@ class Menu extends Phaser.Scene {
 
     preload() {
         // load audio
-        this.load.audio('sfx_select', './assets/bloopbleep.wav');
+        this.load.audio('sfx_select', './assets/select_bloop.wav');
         this.load.audio('sfx_twinkle1', './assets/twinkle1.wav');
         this.load.audio('sfx_twinkle2', './assets/twinkle2.wav');
         this.load.audio('sfx_twinkle3', './assets/twinkle3.wav');
         this.load.audio('sfx_twinkle4', './assets/twinkle4.wav');
         this.load.audio('sfx_beelaunch', './assets/plink.wav');
+        // load title screen
+        this.load.image('title', 'assets/Rocket Petal Title Screen.png');
     }
 
     create() {
-        // display menu
-        let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 0
-    
-        }
 
-        //show menu text
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+        //display title screen
+        this.title = this.add.tileSprite(0, 0 , 640, 480,'title').setOrigin(0,0);
 
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
